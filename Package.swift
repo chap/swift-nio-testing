024217e93e6889989379b77472589746b6e055a0
Chap Ambrose <chapambrose@gmail.com>
2019-01-10 09:44:18 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,8 @@
-dependencies: [
-    .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0")
-]
+import PackageDescription
+
+let package = Package(
+    name: "swift-nio",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0")
+    ]
+)
